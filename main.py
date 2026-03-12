8034cb2055505d0a8762e695d328ee0ea62e8a52
SrWyatt <80370649+SrWyatt@users.noreply.github.com>
2026-03-12 09:39:53 -0600

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import json
-import time
-
-JSON_PATH = "now_playing.json"
-
-while True:
-    with open(JSON_PATH, 'w') as f:
-        json.dump({"song": "WYATT STATION - BROADCAST"}, f)
-    time.sleep(60)
